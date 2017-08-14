d70c9199d9a13e23d5794fcbcdc003f3f88910d8
LanternControl <brett4president@hotmail.com>
2017-08-14 16:02:30 -0400

    Got three down

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
+const chalk = require('chalk');
 let moment = require('moment');
 moment().format();
 
-let now = moment().format("dddd, MMMM Do YYYY, hh:mm:ss A");
+//ty lyman for the help here
+let mmt = moment();
+
+let now = chalk.blue(moment().format("dddd, MMMM Do YYYY, hh:mm:ss A"));
 
 console.log("It is " + now);
+
+let now1 = chalk.red(moment().format("DDD"));
+
+console.log("It is the " + now1 + " day of the year.");
+let secondsInDay = mmt.diff(moment().hour(0).minute(0).second(0),"seconds");
+let now2 = chalk.blue(moment().format("sssss"));
+
+console.log("It is the " + secondsInDay + " seconds into the day.")
