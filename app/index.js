44a1845f72a107673a88dbf9270e93c22fd7b3eb
LanternControl <brett4president@hotmail.com>
2017-08-14 16:18:38 -0400

    Working on if statement

--- app/index.js
+++ app/index.js
@@ -9,10 +9,17 @@ let now = chalk.blue(moment().format("dddd, MMMM Do YYYY, hh:mm:ss A"));
 
 console.log("It is " + now);
 
-let now1 = chalk.red(moment().format("DDD"));
+let now1 = chalk.magenta(moment().format("DDD"));
 
 console.log("It is the " + now1 + " day of the year.");
-let secondsInDay = mmt.diff(moment().hour(0).minute(0).second(0),"seconds");
-let now2 = chalk.blue(moment().format("sssss"));
 
-console.log("It is the " + secondsInDay + " seconds into the day.")
+let secondsInDay = chalk.blueBright(mmt.diff(moment().hour(0).minute(0).second(0),"seconds"));
+
+console.log("It is " + secondsInDay + " seconds into the day.")
+
+let savings = chalk.green(moment().isDST());
+if(moment().isDST(); === true;
+return "is"
+
+
+console.log("It " + savings + " during Daylight Savings Time.");
